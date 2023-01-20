bfcad0520ad879e9eb1161415daed4a4ec496584
asheingold <2411740+asheingold@users.noreply.github.com>
2023-01-20 12:09:13 -0500

    Fixes and changes

--- 102/colors.py
+++ 102/colors.py
@@ -2,10 +2,11 @@ VALID_COLORS = ['blue', 'yellow', 'red']
 
 NOT_VALID_STRING = 'Not a valid color'
 
+
 def print_colors():
     """In the while loop ask the user to enter a color,
-       lowercase it and store it in a variable. Next check: 
-       - if 'quit' was entered for color, print 'bye' and break. 
+       lowercase it and store it in a variable. Next check:
+       - if 'quit' was entered for color, print 'bye' and break.
        - if the color is not in VALID_COLORS, print 'Not a valid color' and continue.
        - otherwise print the color in lower case."""
     while True:
@@ -15,7 +16,7 @@ def print_colors():
         if input_color == 'quit':
             print("bye")
             break
-        
+
         elif input_color in VALID_COLORS:
             print(input_color)
 
